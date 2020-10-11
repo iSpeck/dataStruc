9ea0967b14da73cd423743fdff20c06d0964d845
Carlos Leiva <carlos@Carloss-MacBook-Pro.local>
2020-10-10 22:44:52 -0500

    Finished the problem

--- RandSwap.cpp
+++ RandSwap.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
-#include <algorithm>
+//#include <algorithm>
+
 using namespace std;
 
 /* Your function should work for int vectors v of any 
@@ -10,24 +11,31 @@ element of v down to 1, a random integer r ranging from 0 to i is generated.
  The function rand returns a random integer, 
  and we use the fact that rand()%(i+1) generates a random number between 0 and i. */
 
+void cardNumber(int card){
 
-int main(int argc, char const *argv[]){
-    int cards[] = {1,2,3,4,5,6,7,8,9,10};
-    vector<int> v;
-    v.assign(cards,cards+10);
-for (int i = 9; i >= 0; i--){
-    cout <<"Vector before swap: " << v[i]<< endl;
-    int r = rand()% (i+1);
-
-    swap(v[i],r);
-
-   // cout << "Random Int:" << r <<endl;
-    //cout << "\n";
+        vector<int> v; //declares a vector
     
-    cout << "Vector swapped: " << v[i] << endl;
-    cout << "----------------" << endl;
+        for (int i = 0; i <= card;i++ ){ // loops and adds an element into the vector
+       
+        v.push_back(i+1); //assigns the array of cards to the vector
+        }
+
+        srand (time(NULL)); // syncs the time of computer to the rand function
+
+        for (int i = card-1; i >= 0; i--){ //loops down from the ith element -1 
+        int r = rand()% (i+1);
+        swap(v[i],v[r]); // swaps the ith element with the random element
+
+        }
+
+        for (int k =0; k < card; k++){ //prints the output to the screen each time.
+            cout <<" " << v[k];}
+    
 }
 
 
+
+int main(int argc, char const *argv[]){
+    cardNumber(52); //outputs the function
     return 0;
 }
